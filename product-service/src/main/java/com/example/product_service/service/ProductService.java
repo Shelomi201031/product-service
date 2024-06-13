22c25bdf202a506d1da909eb0a2aa0580f101379
Shelomi201031 <shelomi20nipunika@gmail.com>
2024-06-13 12:12:22 +0530

    Product Response dto is implemented

--- product-service/src/main/java/com/example/product_service/service/ProductService.java
+++ product-service/src/main/java/com/example/product_service/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.product_service.service;
 
 import com.example.product_service.dto.ProductRequest;
+import com.example.product_service.dto.ProductResponse;
 import com.example.product_service.model.Product;
 import com.example.product_service.repository.ProductRepositorty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +28,20 @@ public class ProductService {
         productRepositorty.save(product);
         //use this one or below one//log.info("Product " + product.getId() +"is saved" );
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepositorty.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
